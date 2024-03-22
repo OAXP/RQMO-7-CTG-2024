1efcc70c9118890553e3d3a43bda3c1cb8caeff4
oussaadaboss <65104139+oussaadaboss@users.noreply.github.com>
2024-03-21 22:17:28 -0400

    Frontend: added routers and home page base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
-import { Button } from '@chakra-ui/react';
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
+const Home = lazy(() => import('@src/pages/Home'));
+
+const routes = [
+	{ path: '/home', element: <Home /> },
+	{ path: '/', element: <Home /> },
+];
 
 function App() {
-	return <Button>RQMO TEAM 7</Button>;
+	return (
+		<Suspense>
+			<BrowserRouter>
+				<Routes>
+					{routes.map((route, index) => (
+						<Route key={index} path={route.path} element={route.element} />
+					))}
+				</Routes>
+			</BrowserRouter>	
+		</Suspense>
+	);
+	
 }
 
 export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import {Text} from '@chakra-ui/react';
+
+export default function Home() {
+	return (
+        <Text>Home page</Text>
+    );
+}
