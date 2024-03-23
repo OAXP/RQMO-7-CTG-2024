330f75d673244a822b4eb5f3644424840b6c4687
andytama <andytran057@gmail.com>
2024-03-23 10:51:08 -0400

    contact us page

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,11 +1,169 @@
-import { Text } from '@chakra-ui/react';
+import {
+	Box,
+	Button,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Input,
+	InputGroup,
+	InputLeftElement,
+	Text,
+	Textarea,
+	VStack,
+} from '@chakra-ui/react';
+// eslint-disable-next-line import/default
+import React, { useContext, useRef } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BsPerson, BsFillTelephoneFill, BsTelephone } from 'react-icons/bs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { MdOutlineEmail } from 'react-icons/md';
 import Navbar from '@src/layouts/navbar';
+import { colors } from '@src/Theme';
 
 export default function Contact() {
+	const form = useRef<HTMLFormElement>(null);
 	return (
 		<>
-			<Navbar />
-			<Text>Contact page</Text>
+			<Navbar/>
+			<Flex
+				direction={'column'}
+				id={'contact'}
+				minH={'100vh'}
+				bgSize={'100% auto'}
+				bgRepeat={'no-repeat'}
+				backgroundColor={colors.background}
+			>
+				<VStack pt={5}>
+					<Flex
+						m={{ base: 5, md: 16, lg: 10 }}
+						p={{ base: 5, lg: 16 }}
+						w={'100%'}
+						justify={'center'}
+					>
+						<Box
+							bg={'primary'}
+							borderRadius={'lg'}
+							p={8}
+							backgroundColor={'white'} // Modified background color
+							shadow={'base'}
+							w={{ base: '100%', lg: '60%' }}
+						>
+							<Heading fontSize={{ base: '4xl', md: '5xl' }} paddingBottom={5}>Nous joindre</Heading>
+							<Flex align="center">
+								<BsFillTelephoneFill/>
+								<Text fontSize={{ base: 'lg' }} fontWeight="500" ml={2}>Sans frai: 1-888-822-2854</Text>
+							</Flex>
+							<Flex align="center">
+								<BsFillTelephoneFill/>
+								<Text fontSize={{ base: 'lg' }} fontWeight="500" ml={2}>Région Sherbrooke, Qc: (819) 943-2854</Text>
+							</Flex>
+							<Flex align="center" mb={3}>
+								<MdOutlineEmail/>
+								<Text fontSize={{ base: 'lg' }} fontWeight="500" ml={2}>info@rqmo.org</Text>
+							</Flex>
+							<form ref={form}>
+								<VStack spacing={5}>
+									<FormControl isRequired>
+										<FormLabel textColor={'palette.black'}>Nom</FormLabel>
+
+										<Flex alignItems="flex-start" gap={4}>
+											<InputGroup>
+												<InputLeftElement
+												color={'palette.primary'}
+												children={<BsPerson />}
+												/>
+												<Input
+												type="text"
+												name="first_name"
+												placeholder="Prénom"
+												focusBorderColor={'palette.primary'}
+												textColor={'palette.black'}
+												/>
+											</InputGroup>
+
+											<InputGroup>
+												<InputLeftElement
+												color={'palette.primary'}
+												children={<BsPerson />}
+												/>
+												<Input
+												type="text"
+												name="last_name"
+												placeholder="Nom"
+												focusBorderColor={'palette.primary'}
+												textColor={'palette.black'}
+												/>
+											</InputGroup>
+										</Flex>
+									</FormControl>
+
+									<FormControl isRequired>
+										<FormLabel textColor={'palette.black'}>Courriel</FormLabel>
+
+										<InputGroup>
+											<InputLeftElement
+												color={'palette.primary'}
+												children={<MdOutlineEmail />}
+											/>
+											<Input
+												type="email"
+												name="user_email"
+												placeholder="Votre courriel"
+												focusBorderColor={'button'}
+												textColor={'palette.black'}
+											/>
+										</InputGroup>
+									</FormControl>
+
+									<FormControl isRequired>
+										<FormLabel textColor={'palette.black'}>Telephone</FormLabel>
+										<InputGroup>
+											<InputLeftElement
+												color={'palette.primary'}
+												children={<BsTelephone />}
+											/>
+											<Input
+												type="email"
+												name="user_email"
+												placeholder="Numéro de téléphone"
+												focusBorderColor={'button'}
+												textColor={'palette.black'}
+											/>
+										</InputGroup>
+									</FormControl>
+
+									<FormControl isRequired>
+										<FormLabel textColor={'palette.black'}>Message</FormLabel>
+
+										<Textarea
+											name="message"
+											placeholder="Votre Message"
+											rows={6}
+											resize="none"
+											focusBorderColor={'button'}
+											textColor={'palette.black'}
+										/>
+									</FormControl>
+
+									<Button
+										colorScheme="blue"
+										bg={'button'}
+										color="white"
+										_hover={{
+											bg: 'button_hover',
+										}}
+										w={'full'}
+										type={'submit'}
+									>
+										Envoyer Message
+									</Button>
+								</VStack>
+							</form>
+						</Box>
+					</Flex>
+				</VStack>
+			</Flex>
 		</>
 	);
 }
