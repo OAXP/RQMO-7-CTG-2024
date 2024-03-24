3bcd55eaff66ed2f3964d8474b842e898abaa3c0
Louis Xu <louisxu2003@gmail.com>
2024-03-23 20:05:44 -0400

    dynamically changing char

--- frontend/src/components/ui/GameBody.tsx
+++ frontend/src/components/ui/GameBody.tsx
@@ -1,35 +1,35 @@
-import { Button, SimpleGrid } from '@chakra-ui/react';
+import { Button, SimpleGrid } from "@chakra-ui/react";
 import IDisease from "@src/types/disease";
 
 function GameBody({ disease }: { disease: IDisease }) {
-	const getSymptom = (part: string) => {
-		const symptom = disease.symptoms.find((s) => s.part === part);
-		window.alert(symptom ? symptom.description : "I don't feel anything here.");
-		return symptom ? symptom.description : "I don't feel anything here.";
-	};
+  const getSymptom = (part: string) => {
+    const symptom = disease.symptoms.find((s) => s.part === part);
+    window.alert(symptom ? symptom.description : "I don't feel anything here.");
+    return symptom ? symptom.description : "I don't feel anything here.";
+  };
 
-	const parts = [
-		'head',
-		'eyes',
-		'mouth',
-		'neck',
-		'chest',
-		'heart',
-		'arms',
-		'belly',
-		'crotch',
-		'legs',
-	];
+  const parts = [
+    "head",
+    "eyes",
+    "mouth",
+    "neck",
+    "chest",
+    "heart",
+    "arms",
+    "belly",
+    "crotch",
+    "legs",
+  ];
 
-	return (
-		<SimpleGrid columns={2} spacing={10}>
-			{parts.map((part) => (
-				<Button key={part} onClick={() => getSymptom(part)}>
-					{part.charAt(0).toUpperCase() + part.slice(1)}
-				</Button>
-			))}
-		</SimpleGrid>
-	);
+  return (
+    <SimpleGrid columns={2} spacing={10}>
+      {parts.map((part) => (
+        <Button key={part} onClick={() => getSymptom(part)}>
+          {part.charAt(0).toUpperCase() + part.slice(1)}
+        </Button>
+      ))}
+    </SimpleGrid>
+  );
 }
 
 export default GameBody;
--- frontend/src/features/ThreeGame.tsx
+++ frontend/src/features/ThreeGame.tsx
@@ -3,8 +3,22 @@ import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 import { Desk } from "@src/models/Desk";
 import { Notebook } from "@src/models/Notebook";
+import { Person } from "@src/models/Person";
+import { Html } from "@react-three/drei";
+import { Button } from "@chakra-ui/react";
+import { colors } from "@src/Theme";
 
 const ThreeGame = () => {
+  const [personNumber, setPersonNumber] = React.useState(76);
+
+  function handlePersonNumberChange() {
+    if (personNumber <= 85) {
+      setPersonNumber(personNumber + 1);
+    } else {
+      setPersonNumber(76);
+    }
+  }
+
   const adjustDeskForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [4, 2, 2];
@@ -17,24 +31,44 @@ const ThreeGame = () => {
     return [screenScale, screenPosition, rotation];
   };
 
+  const adjustPerson = () => {
+    let personNumber = Math.random() * 282;
+    personNumber = 12;
+    let rotation = 1;
+    return [Math.floor(personNumber), rotation];
+  };
+
   const [deskScale, deskPosition, deskRotation] = adjustDeskForScreenSize();
 
   return (
     <Canvas camera={{ position: [5, 4, 5] }}>
-      <ambientLight intensity={1} />
-      <pointLight position={[3.5, 2.5, 2.5]} intensity={0.3} />
-      <mesh position={[0, 4, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial
-          color={new THREE.Color("lightblue")}
-          wireframe={true}
-        />
+      <Html position={[0, 4, 0]}>
+        <Button
+          _hover={{ backgroundColor: colors.button_hover }}
+          backgroundColor={"white"}
+          width={"100%"}
+          borderColor={colors.Primary}
+          borderWidth={"2px"}
+          borderRadius={"20px"}
+          marginBottom={"20px"}
+          onClick={handlePersonNumberChange}
+        >
+          increment character : {personNumber}
+        </Button>
+      </Html>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 2.5, 2.5]} intensity={0.7} />
+      {/* floor */}
+      <mesh position={[0, 0, 0]} rotation={[0, 0, 0]} receiveShadow>
+        <boxGeometry args={[15, 0, 15]} />
+        <meshStandardMaterial color={new THREE.Color("lightblue")} />
       </mesh>
       <Desk position={deskPosition} scale={deskScale} rotation={deskRotation} />
       <Notebook
         scale={[0.004, 0.004, 0.004]}
         position={[3.3, 2.5, 3.3]}
       ></Notebook>
+      <Person personNumber={personNumber}></Person>
       <gridHelper></gridHelper>
     </Canvas>
   );
