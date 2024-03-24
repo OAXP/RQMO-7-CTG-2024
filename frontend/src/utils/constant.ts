f55b11381d51475c6e433fd58e747e1a5d23327a
AmiraTamakloe <amira.tamakloe@polymtl.ca>
2024-03-24 13:35:34 -0400

    url correction

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -2,5 +2,4 @@ import IDisease from '@src/types/disease';
 import Symptom from '@src/types/symptom';
 import { Question } from '@src/interfaces/Question';
 
-export const SERVER_URL = 'http://localhost:3000';
 export type dataInterface = IDisease | Symptom | Question;
