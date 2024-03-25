360a1ad3172804133b678e3fabf57318ba2e0ca3
Rayan Ait Abdesselam <rayan.ait-abdesselam@polymtl.ca>
2024-03-24 22:41:13 -0400

    fix lint errors

--- frontend/src/models/Computer.tsx
+++ frontend/src/models/Computer.tsx
@@ -6,36 +6,34 @@ Source: https://sketchfab.com/3d-models/retro-computer-setup-free-82eaf2047e0447
 Title: Retro Computer Setup (FREE)
 */
 
-import * as THREE from "three";
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-import { GLTF } from "three-stdlib";
-import computerScene from "@assets/3d/computer.glb";
+import * as THREE from 'three';
+import React, { useRef } from 'react';
+import { useGLTF } from '@react-three/drei';
+import { GLTF } from 'three-stdlib';
+import computerScene from '@assets/3d/computer.glb';
 
 type GLTFResult = GLTF & {
-  nodes: {
-    retro_computer_setup_retro_computer_setup_Mat_0: THREE.Mesh;
-  };
-  materials: {
-    retro_computer_setup_Mat: THREE.MeshPhysicalMaterial;
-  };
+	nodes: {
+		retro_computer_setup_retro_computer_setup_Mat_0: THREE.Mesh;
+	};
+	materials: {
+		retro_computer_setup_Mat: THREE.MeshPhysicalMaterial;
+	};
 };
 
-export function Computer(props: JSX.IntrinsicElements["group"]) {
-  const { nodes, materials } = useGLTF(computerScene) as GLTFResult;
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={
-          nodes.retro_computer_setup_retro_computer_setup_Mat_0.geometry
-        }
-        material={materials.retro_computer_setup_Mat}
-        rotation={[-Math.PI / 2, 0, 0]}
-      />
-    </group>
-  );
+export function Computer(props: JSX.IntrinsicElements['group']) {
+	const { nodes, materials } = useGLTF(computerScene) as GLTFResult;
+	return (
+		<group {...props} dispose={null}>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.retro_computer_setup_retro_computer_setup_Mat_0.geometry}
+				material={materials.retro_computer_setup_Mat}
+				rotation={[-Math.PI / 2, 0, 0]}
+			/>
+		</group>
+	);
 }
 
-useGLTF.preload("/retro_computer_setup_free.glb");
+useGLTF.preload('/retro_computer_setup_free.glb');
--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,18 +1,18 @@
-import { Box, Button, Flex, Input } from "@chakra-ui/react";
-import Navbar from "@src/layouts/navbar";
-import ThreeGame from "@src/features/ThreeGame";
+import { Box, Button, Flex, Input } from '@chakra-ui/react';
+import Navbar from '@src/layouts/navbar';
+import ThreeGame from '@src/features/ThreeGame';
 
 export default function Game() {
-  return (
-    <>
-      <Navbar />
-      <Box h={"full"} w={"full"}>
-        <Flex>
-          <Box height={window.innerWidth < 768 ? 600 : 1000} flex={1}>
-            <ThreeGame></ThreeGame>
-          </Box>
-        </Flex>
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Box h={'full'} w={'full'}>
+				<Flex>
+					<Box height={window.innerWidth < 768 ? 600 : 1000} flex={1}>
+						<ThreeGame></ThreeGame>
+					</Box>
+				</Flex>
+			</Box>
+		</>
+	);
 }
